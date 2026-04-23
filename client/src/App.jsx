6ea5d3f0fdd940959b56fe3dee241dc974900950
MayanthaS <sapumalmayantha@gmail.com>
2026-04-23 16:04:17 +0530

    feat: add login routes, payslip pages, and app layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import { Toaster } from "react-hot-toast";
+import { Routes } from "react-router-dom";
 import LogingLanding from "./pages/LogingLanding";
-import { Route } from "react-router-dom";
-import { Layout, Settings } from "lucide-react";
+import LoginForm from "./components/LoginForm";
+import { Navigate, Route } from "react-router-dom";
+// import { Settings } from "lucide-react";
+import Settings from "./pages/Settings";
+import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
 import Employees from "./pages/Employees";
 import Attendence from "./pages/Attendence";
+import Payslips from "./pages/Payslips";
 import Leave from "./pages/Leave";
+import PrintPayslip from "./pages/PrintPayslip";
 
 const App = () => {
   return (
@@ -13,13 +19,19 @@ const App = () => {
       <Toaster />
       <Routes>
         <Route path="/login" element={<LogingLanding />} />
+
+        <Route path="/login/admin" element={<LoginForm />} />
+        <Route path="/login/employee" element={<LoginForm />} />
         <Route element={<Layout />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/employees" element={<Employees />} />
           <Route path="/attendance" element={<Attendence />} />
           <Route path="/leave" element={<Leave />} />
+          <Route path="/payslips" element={<Payslips />} />
           <Route path="/settings" element={<Settings />} />
         </Route>
+        <Route path="/print/payslips/:id" element={<PrintPayslip />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </>
   );
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className="flex h-screen bg-lineart-to-br from-slate-50 via-white to-indigo-50/30">
+      <p>Sidebar</p>
+      <main className="flex-1 overflow-y-auto">
+        <div className="p-4 pt-16 sm:pt-6 lg:p-8 max-w-400 mx-auto">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
