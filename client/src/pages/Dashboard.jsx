28a1c037e9c0781f706a1abb5bec220104f3f700
MayanthaS <sapumalmayantha@gmail.com>
2026-04-26 20:18:31 +0530

    fix: clean up employee dashboard card rendering fix: resolve dashboard render errors

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyEmployeeDashboardData } from "../assets/assets";
+import Loading from "../components/Loading";
+import EmployeeDashboard from "../components/EmployeeDashboard";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [data] = useState(dummyEmployeeDashboardData);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <Loading />;
+  if (!data) {
+    return <p className="text-center py-12 text-slate-400">Failed Load Data</p>;
+  }
+  if (data.role === "ADMIN") {
+    return <div>admin Dashboard</div>;
+  } else {
+    return <EmployeeDashboard data={data} />;
+  }
 };
 
 export default Dashboard;
