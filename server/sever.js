a1471ebe88d13597ae8e58eec30d162dceaec2ec
MayanthaS <sapumalmayantha@gmail.com>
2026-05-06 16:05:21 +0530

    fix(server): add missing newline for code readability

--- server/sever.js
+++ server/sever.js
@@ -7,6 +7,7 @@ import { connectDB } from "./config/db.js";
 // import authRoutes from "./routes/authRoutes.js";
 // import employeeRoutes from "./routes/employeeRoutes.js";
 
+
 dotenv.config();
 
 const app = express();
